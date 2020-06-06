3ac527d65b2998fa36425be7f81044908d1d429c
Chris Mims <chritopherm970@aol.com>
2020-06-06 14:27:35 -0500

    Added data time attributes to each row

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
     
     $('#currentDay').text(currentDay);
 
+    //Set time for each row
+    $("#row8").attr("data-time", moment("8:00 am", "h:mm a").format("HH"));
+    $("#row9").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+    $("#row10").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
+    $("#row11").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
+    $("#row12").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+    $("#row1").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+    $("#row2").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+    $("#row3").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+    $("#row4").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+    $("#row5").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+    console.log(currentTime)
     // Function to determine current time
     function rightNow(){
 
