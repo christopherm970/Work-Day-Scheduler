4f791ca05f3944ca89e2f5e36ec00fbb2344e38b
Chris Mims <chritopherm970@aol.com>
2020-06-06 22:31:05 -0500

    Added way to store and get local storage text and values.

--- script.js
+++ script.js
@@ -28,10 +28,23 @@ $(document).ready(function(){
     }
     });
     
-    
-    // Make color change through for loop
-    $('saveBtn').on('click', function(){
-        // save text to local storage
-    })
+    //Create variable calling on HTML
+    var description = $(".description");
 
+    // Function to pull data and text from local storage
+    $(description).each(function(i, element) {
+        var timeID = $(element).attr("data-time");
+        var description = localStorage.getItem(timeID);
+        $(element).text(description);
+
+
+    // save text to local storage
+    $('.saveBtn').on('click', function(){
+        var timeID = $(this).siblings(".description").attr("data-time");
+        var userInput = $(this).siblings(".description").val();
+        localStorage.setItem(timeID, userInput);
+    });
+
+
+    });
 })
